ef6100aad9c24c2a5367cfab1a5f70c5153f20e0
minukHwang <damin2003@yonsei.ac.kr>
2024-02-13 17:52:45 +0900

    [ALG-003] Try: 백준 문제 11729번 풀이

--- minukHwang/Week003/src/boj_11729_황민욱/Main.java
+++ minukHwang/Week003/src/boj_11729_황민욱/Main.java
@@ -1,5 +1,24 @@
 package boj_11729_황민욱;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	
+	// 어떠한 변수...........
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		hanoiTower(1, 3, N);
+	}
+	
+	public static int hanoiTower(int from, int to, int count) {
+		if(count == 1) {
+			System.out.printf("%d %d\n", from, to);
+		}
+		
+		to = 6 - from -  to;
+		return hanoiTower(from , to, --count);
+	}
 }
