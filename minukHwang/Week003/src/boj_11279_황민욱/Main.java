570f13310a5b02bde863a2830d50982139205ef6
minukHwang <damin2003@yonsei.ac.kr>
2024-02-14 15:17:33 +0900

    [ALG-003] Solve: SWEA 문제 1231번 풀이

--- minukHwang/Week003/src/boj_11279_황민욱/Main.java
+++ minukHwang/Week003/src/boj_11279_황민욱/Main.java
@@ -1,5 +1,50 @@
 package boj_11279_황민욱;
 
+class MaxHeap {
+	int[] heap; // 배열
+	int size;
+	int maxSize;
+
+	public MaxHeap() {
+
+	}
+
+	public MaxHeap(int maxSize) {
+		this.maxSize = maxSize;
+		this.size = 0;
+		heap = new int[this.maxSize + 1];
+		heap[0] = Integer.MAX_VALUE;
+	}
+
+	private int parent(int pos) {
+		return pos / 2;
+	}
+
+	private int leftChild(int pos) {
+		return 2 * pos;
+	}
+
+	private int rightChild(int pos) {
+		return (2 * pos) + 1;
+	}
+
+	private boolean isLeaf(int pos) {
+		return pos > (size / 2) && pos <= size;
+	}
+	
+	private void swap(int fpos, int spos) {
+		int tmp;
+		tmp = heap[fpos];
+		heap[fpos] = heap[spos];
+		heap[spos] = tmp;
+	}
+	
+	private void maxHeapify(int pos) {
+		
+	}
+
+}
+
 public class Main {
 
 }
--- minukHwang/Week003/src/boj_11729_황민욱/Main.java
+++ minukHwang/Week003/src/boj_11729_황민욱/Main.java
@@ -1,5 +1,19 @@
 package boj_11729_황민욱;
 
+/*
+ * [문제 풀이 과정]
+ * *하노이 탑의 규칙을 점화식 처럼 찾아야하는 문제*
+ * 	- 일단 하노이탑은 만약 N개를 1 -> 3으로 옮겨야 한다.
+ * 	- 그러면 젤 큰 마지막이 3으로 가야하기 때문에 N-1개가 2로 가야한다.
+ *  - N-1개가 2로 가야하면 N-2개는 3으로 가야한다.
+ *  - N-2개가 3으로 가야하면 N-3개는 2로 가야한다.
+ *  ...
+ *  - N-1개가 2로 가고 제일 큰 마지막이 3으로 오게 되면 다시 N-1개가 3으로 가야한다.
+ *  - N-1개가 3으로 가려면 N-2개가 1로 가야한다.
+ *  - N-2개가 1로 가려면 N-3개가 3으로 가야한다.
+ *  ...
+ */
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -21,15 +35,27 @@ public class Main {
 		bw.flush();
 	}
 
+	// n -> 하노이 탑의 층의 개수
+	// from -> 출발 위치
+	// to -> 도착 위치
+	// other -> from, to가 아닌 남아 있는 하나의 대안
 	public static void hanoiTower(int n, int from, int to, int other) throws IOException {
+		// 함수를 들어올 때마다 개수를 세면 옮긴 횟수.
 		count++;
+
+		// 기저 조건 -> 만약 1개만 남으면 그냥 옮기면 되므로 옮기는 위치를 print
 		if (n == 1) {
 			bw.write(from + " " + to + "\n");
 			return;
 		}
 
+		// 일단 처음에 제일 큰 하노이 블럭이 목적지로 갈 때까지 재귀적으로 옮기기
 		hanoiTower(n - 1, from, other, to);
+
+		// 제일 큰 블록 옮기기
 		bw.write(from + " " + to + "\n");
+		
+		// 다시 n-1을 목적지로 재귀적으로 옮기기
 		hanoiTower(n - 1, other, to, from);
 	}
 }
