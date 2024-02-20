35ec4c04358752aa4d61b9207e970a09e8676fd5
minukHwang <damin2003@yonsei.ac.kr>
2024-02-20 18:10:23 +0900

    [ALG-004] Solve: SWEA 문제 7964번 풀이

--- minukHwang/Week004/src/swea_7964_황민욱/Solution.java
+++ minukHwang/Week004/src/swea_7964_황민욱/Solution.java
@@ -1,5 +1,54 @@
 package swea_7964_황민욱;
 
-public class Solution {
+import java.util.Scanner;
 
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int t = 1; t <= T; t++) {
+			int cityNum = sc.nextInt();
+			int limit = sc.nextInt();
+			
+			// 지도 정보 받아오기
+			int[] map = new int[cityNum];
+			
+			for(int i = 0; i < cityNum; i++) {
+				map[i] = sc.nextInt();
+			}
+			
+			int index = 0;
+			int count = 0;
+			
+			while(index < cityNum) {
+				boolean isConnected = false;
+				
+				if(map[index] == 0) {
+					map[index] = 1;
+					count++;
+				}
+				
+				
+				for(int i = limit; i >= 1; i--) {
+					if(index + i < cityNum && map[index + i] == 1) {
+						index += i;
+						isConnected = true;
+						break;
+					}
+				}
+				
+				if(isConnected) {
+					continue;
+				}
+				
+				index += limit;
+			}
+			
+			
+			
+			System.out.printf("#%d %d\n", t , count);
+		}
+	}
 }
